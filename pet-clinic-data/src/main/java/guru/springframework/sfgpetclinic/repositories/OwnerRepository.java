eb639d57e6617db56a2f6fafb6029f5b920d62e5
Wojciech Jończyk <wjonczyk@duck.com>
2022-11-06 20:41:54 +0100

    Added find Owner by lastName with LIKE

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
@@ -3,7 +3,9 @@ package guru.springframework.sfgpetclinic.repositories;
 import guru.springframework.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
-public interface OwnerRepository extends CrudRepository<Owner, Long> {
+import java.util.List;
 
+public interface OwnerRepository extends CrudRepository<Owner, Long> {
     Owner findByLastName(String lastName);
+    List<Owner> findAllByLastNameLike(String lastName);
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
@@ -2,7 +2,9 @@ package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.model.Owner;
 
-public interface OwnerService extends CrudService<Owner, Long> {
+import java.util.List;
 
+public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName);
+    List<Owner> findAllByLastNameLike(String lastName);
 }
